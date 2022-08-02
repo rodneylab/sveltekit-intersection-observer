3f07ba0f69ea5f1747e3fb170291a855ac07681a
Rodney Lab <codesign@rodneylab.com>
2022-08-02 06:49:48 +0100

    style: 🛁  switch to tab indentation

--- src/lib/components/IntersectionObserver.svelte
+++ src/lib/components/IntersectionObserver.svelte
@@ -1,45 +1,45 @@
 <script>
-  import { onMount, onDestroy } from 'svelte';
-  import { browser } from '$app/env';
+	import { onMount, onDestroy } from 'svelte';
+	import { browser } from '$app/env';
 
-  function handleView() {
-    alert('Intersection Observer view event triggered');
-  }
+	function handleView() {
+		alert('Intersection Observer view event triggered');
+	}
 
-  let container;
+	let container;
 
-  /**
-   * @type {IntersectionObserver}
-   */
-  let observer;
+	/**
+	 * @type {IntersectionObserver}
+	 */
+	let observer;
 
-  onMount(() => {
-    if (browser) {
-      /**
-       * @param entries {IntersectionObserverEntry[]}
-       * @param observer {IntersectionObserver}
-       */
-      const handleIntersect = (entries, observer) => {
-        entries.forEach((entry) => {
-          if (entry.isIntersecting) {
-            observer.unobserve(entry.target);
-            handleView();
-          }
-        });
-      };
-      const options = { threshold: 1, rootMargin: '100% 0% -100%' };
-      observer = new IntersectionObserver(handleIntersect, options);
-      observer.observe(container);
-    }
-  });
+	onMount(() => {
+		if (browser) {
+			/**
+			 * @param entries {IntersectionObserverEntry[]}
+			 * @param observer {IntersectionObserver}
+			 */
+			const handleIntersect = (entries, observer) => {
+				entries.forEach((entry) => {
+					if (entry.isIntersecting) {
+						observer.unobserve(entry.target);
+						handleView();
+					}
+				});
+			};
+			const options = { threshold: 1, rootMargin: '100% 0% -100%' };
+			observer = new IntersectionObserver(handleIntersect, options);
+			observer.observe(container);
+		}
+	});
 
-  onDestroy(() => {
-    if (observer) {
-      observer.disconnect();
-    }
-  });
+	onDestroy(() => {
+		if (observer) {
+			observer.disconnect();
+		}
+	});
 </script>
 
 <div bind:this={container}>
-  <slot />
+	<slot />
 </div>
